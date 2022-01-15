8c4b2e9b2975fe6befbcf3d34197377964c10486
ArtemEfimovich <artemfender@gmail.com>
2022-01-15 21:52:02 +0300

    gh pages

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,25 @@ import React from 'react';
 import './App.css';
 import Header from "./components/header/Header";
 import RouteComponent from "./routes/RouteComponent";
+import {initializeApp} from "firebase/app";
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDpyqQX4f3rbhPOQjGHLT-BtswBd4lbsQM",
+    authDomain: "store-basket.firebaseapp.com",
+    projectId: "store-basket",
+    storageBucket: "store-basket.appspot.com",
+    messagingSenderId: "203762527651",
+    appId: "1:203762527651:web:640f3c07875454fdf33e16"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+
+
+
+
 
 function App() {
     return (
--- src/bll/store/store.ts
+++ src/bll/store/store.ts
@@ -1,16 +1,14 @@
-import { configureStore } from '@reduxjs/toolkit'
+import {configureStore} from '@reduxjs/toolkit'
 import {actionTypes, firebaseReducer} from "react-redux-firebase";
 import {firestoreReducer} from "redux-firestore";
-import { initializeApp } from "firebase/app";
 import {basketReducer} from "../reducers/basketReducer";
-
 
 
 export const store = configureStore({
     reducer: {
         firebaseReducer,
         firestoreReducer,
-        basket:basketReducer,
+        basket: basketReducer,
     },
     middleware: (getDefaultMiddleware) => getDefaultMiddleware({
         serializableCheck: {
@@ -22,14 +20,3 @@ export const store = configureStore({
 export type AppRootStateType = ReturnType<typeof store.getState>
 
 
-const firebaseConfig = {
-    apiKey: "AIzaSyDpyqQX4f3rbhPOQjGHLT-BtswBd4lbsQM",
-    authDomain: "store-basket.firebaseapp.com",
-    projectId: "store-basket",
-    storageBucket: "store-basket.appspot.com",
-    messagingSenderId: "203762527651",
-    appId: "1:203762527651:web:640f3c07875454fdf33e16"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
